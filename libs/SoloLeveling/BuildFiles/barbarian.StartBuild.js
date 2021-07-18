8e6ee35022e8c410ee3f635bfd9dcf8ec5dbd9a5
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-07-18 03:05:44 -0400

    Update barbarian.StartBuild.js
    -Update skills and clean up old build comment

--- libs/SoloLeveling/BuildFiles/barbarian.StartBuild.js
+++ libs/SoloLeveling/BuildFiles/barbarian.StartBuild.js
@@ -28,20 +28,6 @@ var build = {
 		["dexterity", 136], ["strength", 150], ["vitality", "all"],
 	],
 	skills: [
-		/*[126, 2, false], // Bash 2 // -> charlvl 3
-		[127, 2, false], // Sword Mastery 2 // -> charlvl 4
-		[130, 1, false], // Howl 1 // -> charlvl 5
-		[133, 1, false], // Double Swing 1 // -> charlvl 6
-		[138, 1, false], // Shout 1 // -> charlvl 7
-		[133, 9, false], // Double Swing 9 // -> charlvl 16
-		[146, 1, false], // Battle Cry 1 // -> charlvl 18
-		[145, 1, false], // Iron Skin 1 // -> charlvl 19
-		[126, 6, false], // Bash 6 // -> charlvl 23
-		[149, 1, false], // Battle Orders 1 // -> charlvl 24
-		[126, 20, true], // Bash 20 // -> charlvl 38
-		[153, 5, false], // Natural Resistance 5 // charLvl 43
-		[133, 15, false], // Double Swing 15 // -> charlvl 49
-		[149, 20, false], // Battle Orders 20 // -> charlvl 68*/
 		[126, 1, false], // Bash -> charlvl 2
 		[130, 1, false], // Howl -> charlvl 4
 		[127, 2, false], // Sword Mastery -> charlvl 3 (1 point from den)
@@ -56,28 +42,29 @@ var build = {
 		[149, 1, true], // Battle Orders -> charlvl 24
 		[147, 2, false], // Frenzy -> charlvl 25
 		[149, 2, false], // Battle Orders -> charlvl 26
-		[147, 3, false], // Frenzy -> charlvl 27
-		[149, 3, false], // Battle Orders -> charlvl 28
+		[147, 3, false], // Frenzy -> charlvl 26
+		[149, 3, false], // Battle Orders -> charlvl 27
+		[147, 4, false], // Frenzy -> charlvl 29
 		[155, 1, true], // Battle Command -> charlvl 30
 		[154, 1, true], // War Cry -> charlvl 30
 		[153, 5, true], // Natural Resistance 5 // charLvl 35 (enough res to get out or normal)
-		[147, 4, false], // Frenzy -> charlvl 36
+		[147, 5, false], // Frenzy -> charlvl 36
 		[149, 4, false], // Battle Orders -> charlvl 37
-		[147, 5, false], // Frenzy -> charlvl 38
+		[147, 6, false], // Frenzy -> charlvl 38
 		[149, 5, false], // Battle Orders -> charlvl 39
-		[147, 6, false], // Frenzy -> charlvl 40
+		[147, 7, false], // Frenzy -> charlvl 40
 		[149, 6, false], // Battle Orders -> charlvl 41
-		[147, 7, false], // Frenzy -> charlvl 42
+		[147, 8, false], // Frenzy -> charlvl 42
 		[149, 7, false], // Battle Orders -> charlvl 43
-		[147, 8, false], // Frenzy -> charlvl 44
+		[147, 9, false], // Frenzy -> charlvl 44
 		[149, 8, false], // Battle Orders -> charlvl 45
-		[147, 9, false], // Frenzy -> charlvl 46
+		[147, 10, false], // Frenzy -> charlvl 46
 		[149, 9, false], // Battle Orders -> charlvl 47
-		[147, 10, false], // Frenzy -> charlvl 48
+		[147, 11, false], // Frenzy -> charlvl 48
 		[149, 10, false], // Battle Orders -> charlvl 49
-		[147, 11, false], // Frenzy -> charlvl 50
+		[147, 12, false], // Frenzy -> charlvl 50
 		[149, 11, false], // Battle Orders -> charlvl 51
-		[147, 12, false], // Frenzy -> charlvl 52
+		[147, 13, false], // Frenzy -> charlvl 52
 		[149, 12, false], // Battle Orders -> charlvl 53 (BO now lasts 2 minutes, so lets max Frenzy)
 		[147, 20, false], // Frenzy -> charlvl 61
 		[149, 20, false], // Battle Orders -> charlvl 69
