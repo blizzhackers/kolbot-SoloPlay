6dd1bc35701ecb18ece273a8a109af77fa9a66d7
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-07-19 13:47:20 -0400

    Add AutoMuleOverrides
    -Don't mule wanted autoequip items
    -Fix barb stuttering when precasting before level 30

---
+++ libs/SoloLeveling/Functions/AutoMuleOverrides.js
@@ -0,0 +1,42 @@
+/*
+*	@filename	AutoMuleOverrides.js
+*	@author		theBGuy
+*	@desc		Automule fix to not mule wanted autoequip items
+*/
+
+if (!isIncluded("Automule.js")) {
+	include("Automule.js");
+}
+
+AutoMule.getMuleItems = function () {
+	var item, items,
+		info = this.getInfo();
+
+	if (!info || !info.hasOwnProperty("muleInfo")) {
+		return false;
+	}
+
+	item = me.getItem(-1, 0);
+	items = [];
+
+	if (item) {
+		do {
+			if (Town.ignoredItemTypes.indexOf(item.itemType) === -1 &&
+				(Town.questItemClassids.indexOf(item.classid) === -1) && // Don't mule quest items
+					(Pickit.checkItem(item).result > 0 || (item.location === 7 && info.muleInfo.hasOwnProperty("muleOrphans") && info.muleInfo.muleOrphans)) &&
+					(!Item.autoEquipKeepCheck(item) && !Item.autoEquipCheckSecondary(item) && !Item.autoEquipKeepCheckMerc(item) && !Item.autoEquipCharmCheck(item)) &&
+					item.classid !== 549 && // Don't drop Horadric Cube
+					(item.classid !== 603 || item.quality !== 7) && // Don't drop Annihilus
+					(item.classid !== 604 || item.quality !== 7) && // Don't drop Hellfire Torch
+					(item.location === 7 || (item.location === 3 && !Storage.Inventory.IsLocked(item, Config.Inventory))) && // Don't drop items in locked slots
+					((!TorchSystem.getFarmers() && !TorchSystem.isFarmer()) || [647, 648, 649].indexOf(item.classid) === -1)) { // Don't drop Keys if part of TorchSystem
+				if (this.matchItem(item, Config.AutoMule.Force.concat(Config.AutoMule.Trigger)) || // Always drop items on Force or Trigger list
+					(!this.matchItem(item, Config.AutoMule.Exclude) && (!this.cubingIngredient(item) && !this.runewordIngredient(item) && !this.utilityIngredient(item)))) { // Don't drop Excluded items or Runeword/Cubing/CraftingSystem ingredients
+					items.push(copyUnit(item));
+				}
+			}
+		} while (item.getNext());
+	}
+
+	return items;
+};
--- libs/SoloLeveling/Functions/PrecastOverrides.js
+++ libs/SoloLeveling/Functions/PrecastOverrides.js
@@ -80,15 +80,31 @@ Precast.doPrecast = function (force) {
 
 		break;
 	case 4: // Barbarian - TODO: BO duration
+		var swap = me.weaponswitch;
+
 		if (me.charlvl < 24) {
 			if (me.getSkill(138, 0) && (!me.getState(26) || force) && Skill.getManaCost(138) < me.mp) {
 				Skill.cast(138, 0); // Shout
 
 				break;
 			}
-		} else if (!me.getState(32) || !me.getState(51) || !me.getState(26) || force) {
-			var swap = me.weaponswitch;
+		} else if (me.charlvl < 30) {
+			Attack.weaponSwitch(this.getBetterSlot(149));
 
+			if (me.getSkill(138, 0) && (!me.getState(26) || force) && Skill.getManaCost(138) < me.mp) {
+				Skill.cast(138, 0); // Shout
+
+			}
+
+			if (me.getSkill(149, 0) && (!me.getState(32) || force) && Skill.getManaCost(149) < me.mp) {
+				Skill.cast(149, 0); // Battle Orders
+
+			}
+
+			Attack.weaponSwitch(swap);
+
+			break;
+		} else if (!me.getState(32) || !me.getState(51) || !me.getState(26) || force) {
 			Attack.weaponSwitch(this.getBetterSlot(149));
 
 			if (me.getSkill(155, 0) && (!me.getState(51) || force) && Skill.getManaCost(155) < me.mp) {
