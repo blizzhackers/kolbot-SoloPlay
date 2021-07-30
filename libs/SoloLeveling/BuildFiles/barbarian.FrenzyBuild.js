117dc6f06e5089a3a1ab354cb4d573f8ec950b39
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-07-29 23:03:29 -0400

    Update bo stick line

--- libs/SoloLeveling/BuildFiles/barbarian.FrenzyBuild.js
+++ libs/SoloLeveling/BuildFiles/barbarian.FrenzyBuild.js
@@ -92,7 +92,7 @@ var finalBuild = {
 		"[name] == grandcharm && [quality] == magic # [masteriesskilltab] == 1 # [invoquantity] == 1 && [finalcharm] == true && [charmtier] == 1000 + charmscore(item)",
 		"[name] == grandcharm && [quality] == magic # [barbcombatskilltab] == 1 # [invoquantity] == 1 && [finalcharm] == true && [charmtier] == 1000 + charmscore(item)",
 		//Switch
-		"([type] == club || [type] == sword || [type] == knife || [type] == throwingknife || [type] == mace) && [quality] == magic # [warcriesskilltab] >= 1 && [secondarymindamage] == 0 # [secondarytier] == 100000 + tierscore(item)",
+		"([type] == club || [type] == sword || [type] == knife || [type] == throwingknife || [type] == mace) && [quality] == magic # [warcriesskilltab]+[barbarianskills] >= 1 && [secondarymindamage] == 0 # [secondarytier] == 100000 + tierscore(item)",
 		//merc
 		"[type] == armor && [flag] == runeword # [enhanceddefense] >= 200 && [enhanceddamage] >= 300 # [merctier] == 100000",	//Fortitude
 		"[name] == demonhead && [quality] == unique && [flag] == ethereal # [strength] >= 25 && [enhanceddefense] >= 100 # [merctier] == 50000",	//Eth Andy's
