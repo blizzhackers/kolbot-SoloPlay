1f1bdb932a5d866173f43c7c751d7c3a8b1fb862
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-08-06 13:54:24 -0400

    Update barbarian.StartBuild.js

--- libs/SoloLeveling/BuildFiles/barbarian.StartBuild.js
+++ libs/SoloLeveling/BuildFiles/barbarian.StartBuild.js
@@ -14,30 +14,28 @@ var build = {
 	mercAuraWanted: 98,
 	mercDiff: 1,
 	stats: [
-		["strength", 30], ["dexterity", 27], ["strength", 35], ["vitality", 40],
-        ["strength", 48], ["dexterity", 39], ["vitality", 50],
-        ["strength", 56], ["dexterity", 45], ["vitality", 60],
-        ["strength", 58], ["dexterity", 50], ["vitality", 70],
-        ["strength", 71], ["vitality", "all"],
+		["strength", 35], ["dexterity", 27], ["vitality", 45],
+        ["strength", 48], ["dexterity", 30], ["vitality", 55],
+        ["strength", 55], ["dexterity", 39], ["vitality", 65],
+        ["strength", 60], ["dexterity", 40], ["vitality", 75],
+        ["strength", 71], ["dexterity", 49], ["vitality", "all"],
 	],
 	skills: [
-		[126, 1, false], // Bash -> charlvl 2
-		[130, 1, false], // Howl -> charlvl 3
-		[127, 2, false], // Sword Mastery -> charlvl 4 (1 point from den)
-		[133, 1, false], // Double Swing -> charlvl 6
-		[137, 1, false], // Taunt -> charlvl 6
-		[127, 3, false], // Sword Mastery -> charlvl 8
-		[133, 2, false], // Double Swing -> charlvl 9
-		[127, 4, false], // Sword Mastery -> charlvl 10
-		[133, 4, false], // Double Swing -> charlvl 11
-		[127, 8, false], // Sword Mastery -> charlvl 16
-		[143, 1, true], // Leap Attack -> charlvl 18
-		[146, 1, true], // Battle Cry -> charlvl 18
-		[145, 1, false], // Iron Skin -> charlvl 18
-		[147, 1, false], // Frenzy -> charlvl 24
-		[149, 1, true], // Battle Orders -> charlvl 24
-		[147, 4, false], // Frenzy -> charlvl 24
-		[127, 13, false], // Sword Mastery 29
-		[137, 20, false], // Taunt -> charlvl 6
+		[126, 1], // Bash -> Level 2
+        [130, 1], // Howl -> Level 3
+        [133, 6, false], // Double Swing -> Level 9
+        [127, 5], // Sword Mastery -> Level 13
+        //[141, 1], // Increased Stamina
+        [137, 1], // Taunt  -> Level 14
+        [127, 6], // Sword Mastery -> Level 15
+        [145, 1], // Iron Skin  -> Level 18
+        [146, 1], // Battle Cry  -> Level 18
+        [127, 9], // Sword Mastery
+        [140, 1], // Double Throw
+        [138, 1], // Shout
+        [147, 1], // Frenzy
+        //[148, 1], // Increased Speed
+        [149, 4, false], // Battle Orders
+        [137, 20], // Taunt
 	]
 };
