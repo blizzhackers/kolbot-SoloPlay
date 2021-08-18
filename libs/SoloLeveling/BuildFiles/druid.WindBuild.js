a950f4aae2b5b28882500b5c013ce632ee1a1616
theBGuy <60308670+theBGuy@users.noreply.github.com>
2021-08-18 09:25:43 -0400

    Fix wind druid build
    -Fix switch charge weapon line to use spellsteel for decrep

--- libs/SoloLeveling/BuildFiles/druid.WindBuild.js
+++ libs/SoloLeveling/BuildFiles/druid.WindBuild.js
@@ -1,7 +1,7 @@
 /**
  *    @filename   druid.WindBuild.js
- *	  @author	  isid0re
- *    @desc       Druid wind build for after respecOne
+ *	  @author	  isid0re, theBGuy
+ *    @desc       Druid wind final build
  */
 
 var finalBuild = {
@@ -9,29 +9,15 @@ var finalBuild = {
 	skillstab: 42, // elemental
 	wantedskills: [245, 250, 240], // tornado, hurricane, twister
 	usefulskills: [235], // cyclone armor
-	precastSkills: [235], // Valkyrie
-	mercAuraName: "Blessed Aim",
-	mercAuraWanted: 108,
-	mercDiff: 0,
+	precastSkills: [235], // cyclone armor
+	mercAuraName: "Might",
+	mercAuraWanted: 98,
+	mercDiff: 1,
 	stats: [
-		["dexterity", 35], ["strength", 48], ["vitality", 165], ["strength", 61], ["vitality", 252], ["strength", 156], ["vitality", "all"]
+		["dexterity", 35], ["strength", 156], ["vitality", "all"]
 	],
 	skills: [
-		[221, 1, false], //Raven
-		[227, 1, false], //Summon Spirit Wolf
-		[226, 1, false], //Oak Sage
-		[237, 1, false], //Summon Dire Wolf (Fenris)
-		[247, 1, false], //Summon Grizzly
-		[230, 1, false], //Arctic Blast
-		[235, 1, false], //Cyclone Armor
-		[240, 1, false], //Twister
-		[245, 13, false], //Tornado
-		[250, 6, false], //Hurricane
-		[235, 12, false], //Cyclone Armor
-		[245, 14, false], //Tornado
-		[250, 7, false], //Hurricane
-		[245, 15, false], //Tornado
-		[250, 8, false], //Hurricane
+		[247, 1], //Summon Grizzly
 		[245, 20, false], //Max tornado
 		[250, 20, false], //Max hurricane
 		[235, 20, false], //Max cyclone armor
@@ -42,18 +28,18 @@ var finalBuild = {
 		//weapon
 		"[Type] == mace && [flag] == runeword # [FCR] == 40 # [tier] == 100000", // HotO
 		//Helmet
-		"[Name] == SkySpirit && [Quality] == Unique # [PassiveFirePierce] >= 10 # [tier] == 100000", // ravenlore
+		"[name] == totemicmask && [quality] == unique # [druidskills] == 2 && [shapeshiftingskilltab] == 2 # [tier] == 110000 + tierscore(item)", //Jalal's mane
 		//belt
-		"[name] == spiderwebsash && [quality] == unique && [flag] != ethereal # [enhanceddefense] >= 90 # [tier] == 100000", //arach's
+		"[name] == spiderwebsash && [quality] == unique && [flag] != ethereal # [enhanceddefense] >= 90 # [tier] == 100000 + tierscore(item)", //arach's
 		//boots
-		"[name] == battleboots && [quality] == unique && [flag] != ethereal # [itemmagicbonus] >= 50 # [tier] == 5000", //war traveler
+		"[name] == battleboots && [quality] == unique && [flag] != ethereal # [itemmagicbonus] >= 50 # [tier] == 5000 + tierscore(item)", //war traveler
 		"[name] == scarabshellboots && [quality] == unique # [strength]+[vitality] >= 20 # [tier] == 100000 + tierscore(item)", //sandstorm treks
 		//armor
 		"[type] == armor && [flag] != ethereal && [flag] == runeword # [frw] >= 45 # [tier] == 100000", //Enigma
 		//shield
 		"[Name] == Monarch && [Flag] != Ethereal && [flag] == runeword # [fcr] >= 35 # [tier] == 100000", //spirit shield
 		//gloves
-		"[name] == lightgauntlets && [quality] == unique && [flag] != ethereal # [fcr] >= 20 # [tier] == 100000", //magefist
+		"[name] == lightgauntlets && [quality] == unique && [flag] != ethereal # [fcr] >= 20 # [tier] == 100000 + tierscore(item)", //magefist
 		//ammy
 		"[type] == amulet && [quality] == unique # [strength] == 5 && [coldresist] >= 30 # [tier] == 100000", //maras
 		//rings
@@ -65,7 +51,6 @@ var finalBuild = {
 		"[name] == grandcharm && [quality] == magic # [elementalskilltab] == 1 # [invoquantity] == 2 && [finalcharm] == true && [charmtier] == 1000 + charmscore(item)",
 		//Switch
 		"[name] == crystalsword && [flag] == runeword # [plusskillbattleorders] >= 1 # [secondarytier] == 100000",
-		"([name] == monarch || [type] == auricshields) && [flag] == runeword # [fcr] >= 25 && [maxmana] >= 89 # [secondarytier] == 110000", //spirit
 		//merc
 		"[type] == armor && [flag] == runeword # [enhanceddefense] >= 200 && [enhanceddamage] >= 300 # [merctier] == 100000",	//Fortitude
 		"[name] == demonhead && [quality] == unique && [flag] == ethereal # [strength] >= 25 && [enhanceddefense] >= 100 # [merctier] == 50000",	//Eth Andy's
