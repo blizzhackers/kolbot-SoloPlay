7bf9226dd98de3f81d6a5aab84fa1091ced06375
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-09-11 18:43:59 -0400

    Clean up

--- libs/SoloPlay/BuildFiles/sorceress.LevelingBuild.js
+++ libs/SoloPlay/BuildFiles/sorceress.LevelingBuild.js
@@ -16,23 +16,14 @@ var build = {
 		["energy", 50], ["strength", 48], ["vitality", 165], ["strength", 61], ["vitality", 200], ["strength", 127], ["vitality", 252], ["dexterity", "block"], ["vitality", "all"]
 	],
 	skills: [
-		[36, 1], // Fire Bolt
-		[37, 1], // warmth
-		[40, 1], // Frozen Armor
-		[39, 1], // ice bolt
-		[45, 1], // ice blast
-		[42, 1], // Static
-		[43, 1], // telekensis
-		[41, 1], // Inferno
-		[46, 1], // Blaze
-		[44, 1], // Frost nova
-		[47, 7], // Fireball 7
-		[54, 1], //Teleport
-		[55, 1], // gspike
-		[51, 1], // Firewall
-		[47, 15], // fireball 15
-		[59, 1], // blizzard
-		[56, 1], // meteor
+		// Total skills at respec = 31 (27 skill points + 4 quest skill points)
+		[37, 1], // Warmth -> points left 30
+		[40, 1], // Frozen Armor -> points left 29
+		[42, 1], // Static -> points left 28
+		[54, 1], // Teleport -> points left 26
+		[59, 1], // blizzard -> points left 21
+		[47, 16], // fireball 15 -> points left 5
+		[56, 1], // meteor -> points left 1
 		[61, 1, false], // Fire Mastery
 		[65, 1, false], // cold mastery
 		[47, 20], // fireball 20
