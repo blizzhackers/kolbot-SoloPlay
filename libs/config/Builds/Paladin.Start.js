4add31ec06c7c7facdb86cdd4673aedeb243e151
aim2kill <mguerra408@gmail.com>
2022-02-21 11:49:28 -0500

    Update Paladin.Start.js
    Mana pot not need until lvl 12 for zeal

--- libs/config/Builds/Paladin.Start.js
+++ libs/config/Builds/Paladin.Start.js
@@ -70,13 +70,6 @@ let AutoBuildTemplate = {
 		Update: function () {
 			Config.AttackSkill = [-1, 0, 102, 0, 102, 0, 98];
 			Config.TownHP = me.playertype ? 0 : Config.TownCheck ? 35 : 0;
-			Config.BeltColumn = ["hp", "hp", "mp", "mp"];
-			Config.MinColumn[0] = Config.BeltColumn[0] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
-			Config.MinColumn[1] = Config.BeltColumn[1] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
-			Config.MinColumn[2] = Config.BeltColumn[2] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
-			Config.MinColumn[3] = Config.BeltColumn[3] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
-			Config.HPBuffer = 2;
-			Config.MPBuffer = 6;
 		}
 	},
 
@@ -129,6 +122,13 @@ let AutoBuildTemplate = {
 		StatPoints: [-1, -1, -1, -1, -1],
 		Update: function () {
 			Config.AttackSkill = [-1, 106, 102, 106, 102, 0, 98];
+			Config.BeltColumn = ["hp", "hp", "mp", "mp"];
+			Config.MinColumn[0] = Config.BeltColumn[0] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.MinColumn[1] = Config.BeltColumn[1] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.MinColumn[2] = Config.BeltColumn[2] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.MinColumn[3] = Config.BeltColumn[3] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.HPBuffer = 2;
+			Config.MPBuffer = 6;
 			Config.Charge = true;
 		}
 	},
