9899841755466ad5d27de749024cc2101d607b23
theBGuy <60308670+theBGuy@users.noreply.github.com>
2022-09-06 19:04:57 -0400

    Update PaladinAttacks.js
    - better paladin attacks. If we are using plain attack but have sacrifice and the unit isn't a fallen (waste to use it on them) and we have enough health then use sacrifice instead
    - fix delay for redemption, use Misc.poll instead
    - add using meditation for after attack if we have it and aren't already in that state

--- libs/SoloPlay/Functions/ClassAttackOverrides/PaladinAttacks.js
+++ libs/SoloPlay/Functions/ClassAttackOverrides/PaladinAttacks.js
@@ -7,7 +7,8 @@
 
 includeIfNotIncluded("common/Attacks/Paladin.js");
 
-ClassAttack.doAttack = function (unit = undefined, preattack = false) {
+// eslint-disable-next-line no-unused-vars
+ClassAttack.doAttack = function (unit = undefined, preattack = false, once = false) {
 	if (!unit || !unit.attackable) return Attack.Result.SUCCESS;
 
 	let gid = unit.gid;
@@ -63,18 +64,6 @@ ClassAttack.doAttack = function (unit = undefined, preattack = false) {
 		Skill.switchCast(sdk.skills.BattleCry, {oSkill: true});
 	}
 
-	if (preattack && Config.AttackSkill[0] > 0 && Attack.checkResist(unit, Config.AttackSkill[0]) && (!me.getState(sdk.states.SkillDelay) || !Skill.isTimed(Config.AttackSkill[0]))) {
-		if (unit.distance > Skill.getRange(Config.AttackSkill[0]) || checkCollision(me, unit, sdk.collision.Ranged)) {
-			if (!Attack.getIntoPosition(unit, Skill.getRange(Config.AttackSkill[0]), sdk.collision.Ranged)) {
-				return Attack.Result.FAILED;
-			}
-		}
-
-		Skill.cast(Config.AttackSkill[0], Skill.getHand(Config.AttackSkill[0]), unit);
-
-		return Attack.Result.SUCCESS;
-	}
-
 	if (Attack.getCustomAttack(unit)) {
 		[attackSkill, aura] = Attack.getCustomAttack(unit);
 	} else {
@@ -107,6 +96,10 @@ ClassAttack.doAttack = function (unit = undefined, preattack = false) {
 				aura = Config.AttackSkill[6];
 			}
 		}
+	}
+
+	if (attackSkill === sdk.skills.Attack && !unit.isFallen && Skill.canUse(sdk.skills.Sacrifice) && me.hpPercent > 75) {
+		attackSkill = sdk.skills.Sacrifice;
 	}
 
 	// Low mana skill
@@ -155,21 +148,151 @@ ClassAttack.doAttack = function (unit = undefined, preattack = false) {
 	return result;
 };
 
+ClassAttack.doCast = function (unit, attackSkill = -1, aura = -1) {
+	if (attackSkill < 0) return Attack.Result.CANTATTACK;
+	// unit became invalidated
+	if (!unit || !unit.attackable) return Attack.Result.SUCCESS;
+	me.weaponswitch !== sdk.player.slot.Main && me.switchWeapons(sdk.player.slot.Main);
+	
+	const currSkill = {
+		Hand: Skill.getHand(attackSkill),
+		Range: Skill.getRange(attackSkill)
+	};
+	
+	switch (attackSkill) {
+	case sdk.skills.BlessedHammer:
+		// todo: add doll avoid to other classes
+		if (Config.AvoidDolls && unit.isDoll) {
+			this.dollAvoid(unit);
+			aura > -1 && Skill.setSkill(aura, sdk.skills.hand.Right);
+			Skill.cast(attackSkill, currSkill.Hand, unit);
+
+			return Attack.Result.SUCCESS;
+		}
+
+		// todo: maybe if we are currently surrounded and no tele to just attack from where we are
+		// hammers cut a pretty wide arc so likely this would be enough to clear our path
+		if (!this.getHammerPosition(unit)) {
+			// Fallback to secondary skill if it exists
+			if (Config.AttackSkill[5] > -1 && Config.AttackSkill[5] !== sdk.skills.BlessedHammer && Attack.checkResist(unit, Config.AttackSkill[5])) {
+				return this.doCast(unit, Config.AttackSkill[5], Config.AttackSkill[6]);
+			}
+
+			return Attack.Result.FAILED;
+		}
+
+		if (unit.distance > 9 || !unit.attackable) return Attack.Result.SUCCESS;
+
+		aura > -1 && Skill.setSkill(aura, sdk.skills.hand.Right);
+
+		for (let i = 0; i < 3; i += 1) {
+			Skill.cast(attackSkill, currSkill.Hand, unit);
+
+			if (!unit.attackable || unit.distance > 9 || unit.isPlayer) {
+				break;
+			}
+		}
+
+		return Attack.Result.SUCCESS;
+	case sdk.skills.HolyBolt:
+		if (unit.distance > currSkill.Range + 3 || CollMap.checkColl(me, unit, sdk.collision.Ranged)) {
+			if (!Attack.getIntoPosition(unit, currSkill.Range, sdk.collision.Ranged)) {
+				return Attack.Result.FAILED;
+			}
+		}
+
+		CollMap.reset();
+
+		if (unit.distance > currSkill.Range || CollMap.checkColl(me, unit, sdk.collision.FriendlyRanged, 2)) {
+			if (!Attack.getIntoPosition(unit, currSkill.Range, sdk.collision.FriendlyRanged, true)) {
+				return Attack.Result.FAILED;
+			}
+		}
+
+		if (!unit.dead) {
+			aura > -1 && Skill.setSkill(aura, sdk.skills.hand.Right);
+			Skill.cast(attackSkill, currSkill.Hand, unit);
+		}
+
+		return Attack.Result.SUCCESS;
+	case sdk.skills.FistoftheHeavens:
+		if (!me.skillDelay) {
+			if (unit.distance > currSkill.Range || CollMap.checkColl(me, unit, sdk.collision.FriendlyRanged, 2)) {
+				if (!Attack.getIntoPosition(unit, currSkill.Range, sdk.collision.FriendlyRanged, true)) {
+					return Attack.Result.FAILED;
+				}
+			}
+
+			if (!unit.dead) {
+				aura > -1 && Skill.setSkill(aura, sdk.skills.hand.Right);
+				Skill.cast(attackSkill, currSkill.Hand, unit);
+
+				return Attack.Result.SUCCESS;
+			}
+		}
+
+		break;
+	case sdk.skills.Attack:
+	case sdk.skills.Sacrifice:
+	case sdk.skills.Zeal:
+	case sdk.skills.Vengeance:
+		if (!Attack.validSpot(unit.x, unit.y, attackSkill, unit.classid)) {
+			return Attack.Result.FAILED;
+		}
+		
+		// 3591 - wall/line of sight/ranged/items/objects/closeddoor 
+		if (unit.distance > 3 || checkCollision(me, unit, sdk.collision.WallOrRanged)) {
+			if (!Attack.getIntoPosition(unit, 3, sdk.collision.WallOrRanged, true)) {
+				return Attack.Result.FAILED;
+			}
+		}
+
+		if (unit.attackable) {
+			aura > -1 && Skill.setSkill(aura, sdk.skills.hand.Right);
+			return (Skill.cast(attackSkill, sdk.skills.hand.LeftNoShift, unit) ? Attack.Result.SUCCESS : Attack.Result.FAILED);
+		}
+
+		break;
+	default:
+		if (currSkill.Range < 4 && !Attack.validSpot(unit.x, unit.y, attackSkill, unit.classid)) return Attack.Result.FAILED;
+
+		if (unit.distance > currSkill.Range || checkCollision(me, unit, sdk.collision.Ranged)) {
+			let walk = (attackSkill !== sdk.skills.Smite && currSkill.Range < 4 && unit.distance < 10 && !checkCollision(me, unit, sdk.collision.BlockWall));
+
+			// walk short distances instead of tele for melee attacks. teleport if failed to walk
+			if (!Attack.getIntoPosition(unit, currSkill.Range, sdk.collision.Ranged, walk)) return Attack.Result.FAILED;
+		}
+
+		if (!unit.dead) {
+			aura > -1 && Skill.setSkill(aura, sdk.skills.hand.Right);
+			Skill.cast(attackSkill, currSkill.Hand, unit);
+		}
+
+		return Attack.Result.SUCCESS;
+	}
+
+	Misc.poll(() => !me.skillDelay, 1000, 40);
+
+	return Attack.Result.SUCCESS;
+};
+
 ClassAttack.afterAttack = function () {
 	Precast.doPrecast(false);
 
-	if (Skill.canUse(sdk.skills.Cleansing) && me.getState(sdk.states.Poison)
-		&& me.getMobCount(12, Coords_1.BlockBits.BlockWall) === 0 && Skill.setSkill(sdk.skills.Cleansing, sdk.skills.hand.Right)) {
-		Misc.poll(function () {
-			me.overhead("Delaying for a second to get rid of Poison");
+	if (Skill.canUse(sdk.skills.Cleansing) && me.hpPercent < 85 && me.getState(sdk.states.Poison)
+		&& !me.checkForMobs({range: 12, coll: Coords_1.BlockBits.BlockWall}) && Skill.setSkill(sdk.skills.Cleansing, sdk.skills.hand.Right)) {
+		me.overhead("Delaying for a second to get rid of Poison");
+		Misc.poll(() => (!me.getState(sdk.states.Poison) || me.mode === sdk.player.mode.GettingHit), 1500, 50);
+	}
 
-			return (!me.getState(sdk.states.Poison) || me.mode === sdk.player.mode.GettingHit);
-		}, 1500, 30);
+	if (Skill.canUse(sdk.skills.Meditation) && me.mpPercent < 50 && !me.getState(sdk.states.Meditation)
+		&& Skill.setSkill(sdk.skills.Meditation, sdk.skills.hand.Right)) {
+		Misc.poll(() => (me.mpPercent >= 50 || me.mode === sdk.player.mode.GettingHit), 1500, 50);
 	}
 
 	if (Skill.canUse(sdk.skills.Redemption) && Config.Redemption instanceof Array
 		&& (me.hpPercent < Config.Redemption[0] || me.mpPercent < Config.Redemption[1])
 		&& Attack.checkNearCorpses(me) > 2 && Skill.setSkill(sdk.skills.Redemption, sdk.skills.hand.Right)) {
-		delay(1500);
+		Misc.poll(() => (me.hpPercent >= Config.Redemption[0] && me.mpPercent >= Config.Redemption[1]), 1500, 50);
 	}
 };
