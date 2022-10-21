06a483d4a837e51c929b9eac122c7af63acef456
theBGuy <60308670+theBGuy@users.noreply.github.com>
2022-10-21 12:34:57 -0400

    Update MiscOverrides.js
    - exclude quest items and base items from logging

--- libs/SoloPlay/Functions/MiscOverrides.js
+++ libs/SoloPlay/Functions/MiscOverrides.js
@@ -751,6 +751,7 @@ Misc.logItem = function (action, unit, keptLine) {
 	// should stop logging items unless we wish to see them or it's part of normal pickit
 	if (!nTResult) {
 		switch (true) {
+		case (unit.questItem || unit.isBaseType):
 		case (!unit.isCharm && hasTier && !Developer.debugging.autoEquip):
 		case (charmCheck && !Developer.debugging.smallCharm && unit.classid === sdk.items.SmallCharm):
 		case (charmCheck && !Developer.debugging.largeCharm && unit.classid === sdk.items.LargeCharm):
