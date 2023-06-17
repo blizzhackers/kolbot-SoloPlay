5158f79d01db6a3420fb24c40514af2af5b08fc9
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-06-17 10:51:33 -0400

    Update countess.js
    - better wait time determination for the chest opening after completing the quest

--- libs/SoloPlay/Scripts/countess.js
+++ libs/SoloPlay/Scripts/countess.js
@@ -14,7 +14,9 @@ function countess () {
   Town.doChores(false, { fullChores: true });
   myPrint("starting countess");
 
-  Pather.checkWP(sdk.areas.BlackMarsh, true) ? Pather.useWaypoint(sdk.areas.BlackMarsh) : Pather.getWP(sdk.areas.BlackMarsh);
+  Pather.checkWP(sdk.areas.BlackMarsh, true)
+    ? Pather.useWaypoint(sdk.areas.BlackMarsh)
+    : Pather.getWP(sdk.areas.BlackMarsh);
   Precast.doPrecast(true);
   let forQuest = !Misc.checkQuest(sdk.quest.id.ForgottenTower, sdk.quest.states.Completed);
 
@@ -37,7 +39,26 @@ function countess () {
 
     if (forQuest) {
       Pather.moveToPreset(me.area, sdk.unittype.Object, sdk.objects.SuperChest);
-      delay(3000);
+      let _items = new Set();
+      let _oldSize = 0;
+      let itemEvent = function (gid, mode) {
+        if (gid > 0 && mode === 0) {
+          _items.add(gid);
+        }
+      };
+      try {
+        addEventListener("itemaction", itemEvent);
+        Misc.poll(function () {
+          if (_items.size > _oldSize) {
+            _oldSize = _items.size;
+            return false;
+          }
+
+          return _items.size > 0 && _items.size === _oldSize;
+        }, Time.seconds(5), Time.seconds(1));
+      } finally {
+        removeEventListener("itemaction", itemEvent);
+      }
     }
   } catch (err) {
     console.log("ÿc8Kolbot-SoloPlayÿc0: Failed to kill Countess: " + err);
