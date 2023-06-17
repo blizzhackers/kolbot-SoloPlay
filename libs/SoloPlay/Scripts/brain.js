6817a985767eff8874d0172c759d2c49a5d5c398
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-06-17 10:47:07 -0400

    Update brain.js
    - kill endugu when we get the brain

--- libs/SoloPlay/Scripts/brain.js
+++ libs/SoloPlay/Scripts/brain.js
@@ -9,7 +9,9 @@ function brain () {
   Town.doChores(false, { fullChores: true });
   myPrint("starting brain");
 
-  Pather.checkWP(sdk.areas.FlayerJungle, true) ? Pather.useWaypoint(sdk.areas.FlayerJungle) : Pather.getWP(sdk.areas.FlayerJungle);
+  Pather.checkWP(sdk.areas.FlayerJungle, true)
+    ? Pather.useWaypoint(sdk.areas.FlayerJungle)
+    : Pather.getWP(sdk.areas.FlayerJungle);
   Precast.doPrecast(true);
   Pather.clearToExit(sdk.areas.FlayerJungle, sdk.areas.FlayerDungeonLvl1, Pather.useTeleport());
   Pather.clearToExit(sdk.areas.FlayerDungeonLvl1, sdk.areas.FlayerDungeonLvl2, Pather.useTeleport());
@@ -19,7 +21,7 @@ function brain () {
     console.log("ÿc8Kolbot-SoloPlayÿc0: Failed to get the Brain");
   }
 
-  Attack.clear(0x7);
+  Attack.kill(getLocaleString(sdk.locale.monsters.WitchDoctorEndugu));
   Quest.collectItem(sdk.items.quest.KhalimsBrain, sdk.quest.chest.KhalimsBrainChest);
   Quest.stashItem(sdk.items.quest.KhalimsBrain);
 
